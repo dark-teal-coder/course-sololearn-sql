e4009712830bb96eb1cd705a63eaedeaedc97e46
DarkTealCoder <95575665+dark-teal-coder@users.noreply.github.com>
2022-03-22 17:14:57 +0800

    Add explanation

--- solution-practice-5.2.sql
+++ solution-practice-5.2.sql
@@ -1,2 +1,7 @@
 SELECT DISTINCT Checks 
 FROM Operation; 
+
+/* 
+EXPLANATION: 
+Only the "checks" column is expected. We do not have to include the "id" column. So we cannot use * in this case. SQL is case-insensitive by default so we can write table and column names in PascalCase, all uppercase or lowercase, or mixed case as long as there is no special character in between. 
+*/
