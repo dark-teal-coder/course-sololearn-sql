ac1f4500f299322c8b3b1e83ad74e2993b2f9903
DarkTealCoder <95575665+dark-teal-coder@users.noreply.github.com>
2022-03-22 17:25:55 +0800

    Create solution-practice-5.3.sql

---
+++ solution-practice-5.3.sql
@@ -0,0 +1,11 @@
+SELECT *
+FROM Participants LIMIT 4
+
+-- Alternative:
+SELECT FirstName, LastName
+FROM Participants LIMIT 4
+
+/* 
+EXPLANATION: 
+The expected output shows both first and last names. There are only those 2 columns in the table. So we can use * in this case. SQL is case-insensitive by default so we can write table and column names in PascalCase, all uppercase or lowercase, or mixed case as long as there is no special character in between. 
+*/
