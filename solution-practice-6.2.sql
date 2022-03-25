82fac74167d0fffa4fb837ab742ca79bbd8462b4
DarkTealCoder <95575665+dark-teal-coder@users.noreply.github.com>
2022-03-25 21:11:16 +0800

    Update solution-practice-6.2.sql

--- solution-practice-6.2.sql
+++ solution-practice-6.2.sql
@@ -1,3 +1,13 @@
+SELECT *
+FROM Players 
+ORDER BY losses
+
+-- Alternative:
 SELECT firstname, lastname, losses
 FROM Players 
 ORDER BY losses
+
+/* 
+EXPLANATION: 
+The expected output requires all columns. We can use * or list out all the column nammes. SQL is case-insensitive by default so we can write table and column names in PascalCase, all uppercase or lowercase, or mixed case as long as there is no special character in between. 
+*/
