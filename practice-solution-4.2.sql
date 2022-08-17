03d3ee4e708fda653e6677d33ab7f8ff1ea1e7c9
DarkTealCoder <95575665+dark-teal-coder@users.noreply.github.com>
2022-08-17 11:54:16 +0800

    Put explanation into paragraphs

--- practice-solution-4.2.sql
+++ practice-solution-4.2.sql
@@ -2,6 +2,9 @@ SELECT ProductName, ProductionDate, ExpirationDate
 FROM Products;
 
 /* 
-EXPLANATION: 
-We only have to select 3 columns from the table with 4 columns so we cannot use * in this case. SQL is case-insensitive by default so we can write table and column names in PascalCase, all uppercase or lowercase, or mixed case as long as there is no special character in between. 
+EXPLANATION:
+
+We only have to select 3 columns from the table with 4 columns so we cannot use * in this case. 
+
+SQL is case-insensitive by default so we can write table and column names in PascalCase, all uppercase or lowercase, or mixed case as long as there is no special character in between. 
 */
