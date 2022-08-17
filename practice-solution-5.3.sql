d32ee9536c4b86e9750904c9435588b1b16f2747
DarkTealCoder <95575665+dark-teal-coder@users.noreply.github.com>
2022-08-17 11:58:16 +0800

    Update practice-solution-5.3.sql

--- practice-solution-5.3.sql
+++ practice-solution-5.3.sql
@@ -10,5 +10,5 @@ EXPLANATION:
 
 The expected output shows both first and last names. There are only those 2 columns in the table. So we can use * in this case. 
 
-SQL is case-insensitive by default so we can write table and column names in PascalCase, all uppercase or lowercase, or mixed case as long as there is no special character in between. 
+SQL is case-insensitive by default so we can write table and column names in PascalCase, all UPPERCASE or lowercase, or mixed case as long as there is no special character in between. 
 */
