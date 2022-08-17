d5f200b1ea243d4f0607088d592b54a0cbcc638b
DarkTealCoder <95575665+dark-teal-coder@users.noreply.github.com>
2022-08-17 11:58:56 +0800

    Change to 'UPPERCASE'

--- practice-solution-9.2.sql
+++ practice-solution-9.2.sql
@@ -7,5 +7,5 @@ EXPLANATION:
 
 The expected output does not require all columns. We need to list out the column names one by one. We can use BETWEEN and AND operators to get a range of salaries. 
 
-SQL is case-insensitive by default so we can write table and column names in PascalCase, all uppercase or lowercase, or mixed case as long as there is no special character in between. 
+SQL is case-insensitive by default so we can write table and column names in PascalCase, all UPPERCASE or lowercase, or mixed case as long as there is no special character in between. 
 */
