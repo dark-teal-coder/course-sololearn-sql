bb518e7fa10f633ee9992eab4a6900d7459525a9
DarkTealCoder <95575665+dark-teal-coder@users.noreply.github.com>
2022-08-17 11:58:00 +0800

    Change to 'UPPERCASE'

--- practice-solution-5.2.sql
+++ practice-solution-5.2.sql
@@ -6,5 +6,5 @@ EXPLANATION:
 
 Only the "checks" column is expected. We do not have to include the "id" column. So we cannot use * in this case. 
 
-SQL is case-insensitive by default so we can write table and column names in PascalCase, all uppercase or lowercase, or mixed case as long as there is no special character in between. 
+SQL is case-insensitive by default so we can write table and column names in PascalCase, all UPPERCASE or lowercase, or mixed case as long as there is no special character in between. 
 */
