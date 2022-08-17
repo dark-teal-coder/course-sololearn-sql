e6b1a8b3c27635b0e8ca13cb54eecc6d795de6ed
DarkTealCoder <95575665+dark-teal-coder@users.noreply.github.com>
2022-08-17 11:52:01 +0800

    Update practice-solution-9.2.sql

--- practice-solution-9.2.sql
+++ practice-solution-9.2.sql
@@ -2,5 +2,10 @@ SELECT firstname
 FROM staff 
 WHERE salary BETWEEN 1500 AND 1900 
 
-/*
+/* 
+EXPLANATION:
+
+The expected output does not require all columns. We need to list out the column names one by one. We can use BETWEEN and AND operators to get a range of salaries. 
+
+SQL is case-insensitive by default so we can write table and column names in PascalCase, all uppercase or lowercase, or mixed case as long as there is no special character in between. 
 */
