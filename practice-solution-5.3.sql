6c63ba2a05d2931b2e52379bc2db9b8b9b656be2
DarkTealCoder <95575665+dark-teal-coder@users.noreply.github.com>
2022-08-17 11:52:59 +0800

    Put explanation into paragraphs

--- practice-solution-5.3.sql
+++ practice-solution-5.3.sql
@@ -7,5 +7,8 @@ FROM Participants LIMIT 4
 
 /* 
 EXPLANATION: 
-The expected output shows both first and last names. There are only those 2 columns in the table. So we can use * in this case. SQL is case-insensitive by default so we can write table and column names in PascalCase, all uppercase or lowercase, or mixed case as long as there is no special character in between. 
+
+The expected output shows both first and last names. There are only those 2 columns in the table. So we can use * in this case. 
+
+SQL is case-insensitive by default so we can write table and column names in PascalCase, all uppercase or lowercase, or mixed case as long as there is no special character in between. 
 */
