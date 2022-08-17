c88172277b97967755c6ddc2216a66920cf9c264
DarkTealCoder <95575665+dark-teal-coder@users.noreply.github.com>
2022-08-17 11:52:26 +0800

    Paragraph

--- practice-solution-6.2.sql
+++ practice-solution-6.2.sql
@@ -9,5 +9,8 @@ ORDER BY losses
 
 /* 
 EXPLANATION: 
-The expected output requires all columns. We can use * or list out all the column nammes. SQL is case-insensitive by default so we can write table and column names in PascalCase, all uppercase or lowercase, or mixed case as long as there is no special character in between. 
+
+The expected output requires all columns. We can use * or list out all the column nammes. 
+
+SQL is case-insensitive by default so we can write table and column names in PascalCase, all uppercase or lowercase, or mixed case as long as there is no special character in between. 
 */
