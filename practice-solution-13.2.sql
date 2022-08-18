a1ed322f3a1f994afbe19faf6304edea8b61bbd4
DarkTealCoder <95575665+dark-teal-coder@users.noreply.github.com>
2022-08-18 15:41:04 +0800

    Add explanation

--- practice-solution-13.2.sql
+++ practice-solution-13.2.sql
@@ -1,3 +1,10 @@
 SELECT AVG(Score) FROM Sam_grades
 WHERE Semester = 1
 
+/* 
+EXPLANATION:
+
+The expected output should be the average of Sam's exam scores, which is not already in the table. We need to use AVG() function to calculate the means of scores in semester 1. Here, we use WHERE to specify the condition where semester is 1. 
+
+SQL is case-insensitive by default so we can write table and column names in PascalCase, all UPPERCASE or lowercase, or mixed case as long as there is no special character in between. 
+*/
