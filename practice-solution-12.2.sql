cb354acc91077ce887b2942ab024a0ee0094141a
DarkTealCoder <95575665+dark-teal-coder@users.noreply.github.com>
2022-08-18 10:30:01 +0800

    Add explanation

--- practice-solution-12.2.sql
+++ practice-solution-12.2.sql
@@ -1,3 +1,11 @@
 SELECT CONCAT(Firstname, ' ', Lastname) AS Fullname, Salary*12 + Experience*500 AS Total
 FROM Staff
 ORDER BY Total
+
+/* 
+EXPLANATION:
+
+The expected output requires two customed and renamed columns. The first column requires CONCAT() function to concatenate Firstname and Lastname columns. The second column requires some arithmetic operations. We need to use AS operator to rename columns. 
+
+SQL is case-insensitive by default so we can write table and column names in PascalCase, all UPPERCASE or lowercase, or mixed case as long as there is no special character in between. 
+*/
