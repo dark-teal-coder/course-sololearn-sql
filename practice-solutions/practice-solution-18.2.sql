e8042e72396c8fdd30ece6035f7301571cdd8c23
DarkTealCoder <95575665+dark-teal-coder@users.noreply.github.com>
2022-08-27 11:37:17 +0700

    Add explanation

--- practice-solutions/practice-solution-18.2.sql
+++ practice-solutions/practice-solution-18.2.sql
@@ -2,3 +2,11 @@ SELECT students.ID, students.FirstName, students.LastName, teachers.LastName AS 
 FROM students, teachers
 WHERE students.teacherid=teachers.ID
 ORDER BY students.ID
+
+/* 
+EXPLANATION:
+
+It is easier to start from FROM and WHERE statements. We start by looking at which column(s) contain(s) the same data for us to link them. Both "students" and "teachers" tables share teachers' ID. We can link the two tables using this in WHERE. After that, we can select the columns one by one. Remember to use dot notation to denote table name of each column like this TableName.ColumnName.
+
+SQL is case-insensitive by default so we can write table and column names in PascalCase, all UPPERCASE or lowercase, or mixed case as long as there is no special character in between. 
+*/
