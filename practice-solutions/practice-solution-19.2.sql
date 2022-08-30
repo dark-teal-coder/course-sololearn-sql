da1e32a83c9a2972cf63df5effe7b272d56f9b35
Preesa Saewong <ree.wong.career@gmail.com>
2022-08-30 10:26:21 +0700

    Add explanation

--- practice-solutions/practice-solution-19.2.sql
+++ practice-solutions/practice-solution-19.2.sql
@@ -5,7 +5,7 @@ ON Products.CategoryID=Categories.ID;
 /* 
 EXPLANATION:
 
-xxx
+In the SELECT statement, we need to specify table names as there are 2 tables in one statement. There is no need to leave some cells blank so we use INNER JOIN. For every product, there is a category ID. Use it as a condition to join in the ON statement. 
 
 SQL is case-insensitive by default so we can write table and column names in PascalCase, all UPPERCASE or lowercase, or mixed case as long as there is no special character in between. 
 */
