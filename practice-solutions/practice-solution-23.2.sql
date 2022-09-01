3513251b3df21f6200f4f9085bbe687692deb1ec
Preesa Saewong <ree.wong.career@gmail.com>
2022-09-01 11:05:36 +0700

    Update explanation

--- practice-solutions/practice-solution-23.2.sql
+++ practice-solutions/practice-solution-23.2.sql
@@ -27,6 +27,8 @@ CREATE TABLE TableName
    ColumnName DataType,
 ); 
 
+Check INSERT INTO in lesson 21.
+
 Remember to display the table using the SELECT statement. 
 
 SQL is case-insensitive by default so we can write table and column names in PascalCase, all UPPERCASE or lowercase, or mixed case as long as there is no special character in between. 
