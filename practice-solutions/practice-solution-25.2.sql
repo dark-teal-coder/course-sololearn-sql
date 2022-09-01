769cd4357a5fff79172ca82b91ce0e0544d8e01d
Preesa Saewong <ree.wong.career@gmail.com>
2022-09-01 11:32:04 +0700

    Add explanation to 25.2

--- practice-solutions/practice-solution-25.2.sql
+++ practice-solutions/practice-solution-25.2.sql
@@ -17,7 +17,21 @@ SELECT * from Cities;
 /* 
 EXPLANATION:
 
-xxx
+From the instruction, we need to use ALTER TABLE, UPDATE and SELECT statements. 
+
+Use the following syntax to create a new column in a table: 
+
+ALTER TABLE TableName ADD ColumnName DataType;
+
+Use the following syntax to add values to a column row by row: 
+
+UPDATE TableName
+SET ColumnName=Value
+WHERE Condition;
+
+We add an attractive place to the row where we find a particular city name. 
+
+Remember to display the table using the SELECT statement. 
 
 SQL is case-insensitive by default so we can write table and column names in PascalCase, all UPPERCASE or lowercase, or mixed case as long as there is no special character in between. 
 */
